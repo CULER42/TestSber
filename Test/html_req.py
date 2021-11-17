6718f4b8e8bc000f9b0f3218d207fc7bf9726fb8
CULER42 <Nomad900@mail.ru>
2021-11-17 12:47:09 +0300

    Update html_req.py

--- Test/html_req.py
+++ Test/html_req.py
@@ -2,15 +2,15 @@ from requests_html import HTMLSession
 
 
 
-def get_status_site(*URL):
+def get_status_site(URL):
         html = HTMLSession()
-        get_url = html.get(*URL)
+        get_url = html.get(URL)
         return get_url.status_code
 
 
 def get_canonical(*URL):
         html = HTMLSession()
-        get_url = html.get(*URL)
+        get_url = html.get(URL)
         links = get_url.html.find('link')
         cannonical = ''
         for i in range(len(links)):
